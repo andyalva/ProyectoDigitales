a9c4102c8d0a0a60a3d7d7e74c9ee8b0903455a6
ngonzalez456 <josefegosa@gmail.com>
2018-05-16 10:34:15 -0600

    Add files via upload
    Demux. Bit de VALID_OUT baja cuando elimina un bloque. Falta revisar el formato.

---
+++ miscelaneos/demux/mux_de_control.v
@@ -0,0 +1,22 @@
+module mux_de_control(input [7:0] CONTROL,
+                	input 		CLK,
+                	input 		ENABLE,
+                	input 		DIR,
+			input [1:0] MODO,
+			input 		S_IN,
+			output reg 	S_OUT,
+                        output reg [3:0] Q);
+  
+
+ 
+endmodule
+
+
+	  
+  
+
+ 
+
+
+
+	
